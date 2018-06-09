6e0622cc120bee0fca4344b66d529731ced55670
yahilmadakiya <yahilmadakiya@gmail.com>
2018-06-09 15:47:51 +0530

    Initial Commit: React weather Finder.

---
+++ src/components/Titles.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Titles = () => (
+    <div>
+        <h1 className="title-container__title">Weather Finder</h1>
+        <p className="title-container__subtitle">Find out temperature, conditions and more...</p>
+    </div>
+);
+export default Titles;
